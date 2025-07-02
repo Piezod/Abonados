9d0d70a6cd366bd1e2212b3f0982f99cf75db445
piezod <marte69@gmail.com.com>
2025-07-02 17:26:18 +0200

    Permitir todos los cors

--- src/main/java/com/piezod/abonadoservice/config/WebConfig.java
+++ src/main/java/com/piezod/abonadoservice/config/WebConfig.java
@@ -14,9 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
